806b067bfdbc61245a8b8a9407856b545571bdae
David Laban <alsuren@gmail.com>
2019-12-27 15:39:55 +0000

    Updates

---
+++ precache-manifest.bc45b02573f0f8721d018e46e30371ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "972c97d942d542a6625cded283fd81db",
+    "url": "/alsuren/webrtc-qr/static/media/chime.972c97d9.mp3"
+  },
+  {
+    "revision": "c7fcb3c5d225211f2a2c",
+    "url": "/alsuren/webrtc-qr/static/js/runtime~main.c7fcb3c5.js"
+  },
+  {
+    "revision": "2b537f7e234c4ab0d2e2",
+    "url": "/alsuren/webrtc-qr/static/js/main.2b537f7e.chunk.js"
+  },
+  {
+    "revision": "eafe5aa0f51f3b8f184a",
+    "url": "/alsuren/webrtc-qr/static/js/1.eafe5aa0.chunk.js"
+  },
+  {
+    "revision": "2b537f7e234c4ab0d2e2",
+    "url": "/alsuren/webrtc-qr/static/css/main.64941454.chunk.css"
+  },
+  {
+    "revision": "edfd3a13bf01474ab5558f543dfdea84",
+    "url": "/alsuren/webrtc-qr/index.html"
+  }
+];
